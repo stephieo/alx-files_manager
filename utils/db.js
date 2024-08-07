d15a2076446d8bf3ed2dcd202ce461d652702f47
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-08 00:29:48 +0100

    feat: dbclient with mongodb

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,39 @@
 #!/usr/bin/node
+import MongoClient from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || '27017';
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    this.client = new MongoClient(`mongodb://${host}:${port}`);
+
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(database);
+      })
+      .catch(() => {
+        console.log('Database Connection Failure');
+      });
+  }
+
+  isAlive() {
+    return this.db;
+  }
+
+  async nbUsers() {
+    this.users = this.db.collection('users');
+    const queryAllUsers = await this.users.find({}).toArray();
+    return queryAllUsers.length;
+  }
+
+  async nbFiles() {
+    this.files = this.db.collection('files');
+    const queryAllFiles = await this.files.find({}).toArray();
+    return queryAllFiles.length;
+  }
+}
+
+const dbclient = new DBClient();
+export default dbclient;
