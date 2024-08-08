60ab58690613fe8362a5f02d8a48f0984bd5eb8b
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-08 01:34:42 +0100

    feat: Express server

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+import redisClient from '../utils/redis';
+import dbclient from '../utils/db';
+
+export default class AppController {
+  static getStatus(req, res) {
+    if (redisClient.isAlive() && dbclient.isAlive()) {
+      res.status(200).send({ redis: true, db: true });
+    }
+  }
+
+  static async getStats(req, res) {
+    const totalDbUsers = await dbclient.nbUsers();
+    const totalDbFiles = await dbclient.nbFiles();
+    res.status(200).send({ users: totalDbUsers, files: totalDbFiles });
+  }
+}
--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+import express from 'express';
+import dbRoutes from './routes/index';
+
+const app = express();
+const port = process.env.PORT || '5000';
+
+app.use('/', dbRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
